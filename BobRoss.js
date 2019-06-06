3a75f7cc057cd92dd58ee722ad5f8aa9ade2e5b3
DLeasure <DLeasure9@gmail.com>
2019-06-05 20:17:48 -0400

    buttons working

--- BobRoss.js
+++ BobRoss.js
@@ -1,3 +1,20 @@
+let lastButtonPressed = null;
+
+const redButton = document.querySelector("#red");
+redButton.addEventListener("click", function(e) {
+    lastButtonPressed = "red";
+});
+
+const greenButton = document.querySelector("#green");
+greenButton.addEventListener("click", function(e) {
+    lastButtonPressed = "green";
+});
+
+const blueButton = document.querySelector("#blue");
+blueButton.addEventListener("click", function(e) {
+    lastButtonPressed = "blue";
+});
+
 const squareParent = document.querySelector("#square-parent");
 
 squareParent.addEventListener ("click", function (e) {
